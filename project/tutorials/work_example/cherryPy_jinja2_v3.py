db619b56e3777f52c9f8207385c66c3499610bce
Antonio <cve-anton@yandex.ru>
2015-07-04 04:35:13 -0700

    cherrypy_MVC

---
+++ project/tutorials/work_example/cherryPy_jinja2_v3.py
@@ -0,0 +1,45 @@
+__author__ = 'Antonio'
+
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+import os, os.path
+from pprint import pprint as pp
+
+env = Environment(loader=FileSystemLoader('templates'))
+
+class Root:
+    @cherrypy.expose
+    def index(self):
+        tmpl = env.get_template('index_main.html')
+        return tmpl.render()
+
+    @cherrypy.expose
+    def result(self, queryParameters=None):
+        tmpl = env.get_template('index_content.html')
+        #add chech for several spaces.
+
+        queryParameters_list = queryParameters.strip().split(" ")
+        return tmpl.render(params=queryParameters_list)
+
+
+
+if __name__ == '__main__':
+
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+
+    #need to place it to the conf.
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': 8080,
+                            })
+
+    cherrypy.quickstart(Root(),'/', conf)
+
