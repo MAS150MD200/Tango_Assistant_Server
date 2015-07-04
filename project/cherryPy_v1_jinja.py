fa9def13d47f6ad2567fdded7177e6fe226e466f
Antonio <cve-anton@yandex.ru>
2015-07-03 20:42:14 -0700

    start_playing_with_jinja

---
+++ project/cherryPy_v1_jinja.py
@@ -0,0 +1,14 @@
+__author__ = 'Antonio'
+
+
+import cherrypy
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+        return "Hello world!"
+
+if __name__ == '__main__':
+   cherrypy.quickstart(HelloWorld())
+
+
