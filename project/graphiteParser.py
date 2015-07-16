caee6d6aba739305505c15030b4ba7873561c7ed
Antonio <cve-anton@yandex.ru>
2015-07-16 13:00:34 -0700

    Two_times_faster_gse_search.

--- project/graphiteParser.py
+++ project/graphiteParser.py
@@ -21,22 +21,36 @@ def graphiteFileParser(filename, args):
         pattern_string_list.append("(?=.*" + script_arg + ")")
 
     pattern_string_list.append(".*$")
-    pattern_string = "".join(pattern_string_list)
+    pattern_string = "".join(pattern_string_list)   # result pattern string: ^(?=.*bnq)(?=.*tc2)(?=.*messages)(?=.*6004)(?=.*inbound).*$
+    # DEBUG.
+    # print(pattern_string)
 
     pattern_re = re.compile(pattern_string, re.IGNORECASE)
 
     result = []
 
-    #TODO: edit in python3 style.
-    with codecs.open(filename, mode="r", encoding="utf-8") as fd:
-        for line in fd:
-            if pattern_re.search(line):
-                result.append(line.strip())
+    # Python3 style.
+    with open(filename, mode='r') as fd:
+        file_content = fd.read().split('\n')
+
+    # pp(file_content)
+    for line in file_content:
+        if pattern_re.search(line):
+            result.append(line.strip())
+
+    # TODO: edit in python3 style.
+    # with codecs.open(filename, mode="r", encoding="utf-8") as fd:
+    #     for line in fd:
+    #         if pattern_re.search(line):
+    #             result.append(line.strip())
 
     return result
 
 def main():
     script_args = sys.argv[1:]
+
+    # DEBUG.
+    script_args = "bnq tc2 messages 6004 inbound".split(" ")
     pp(script_args)
     pp(graphiteFileParser(FILE_NAME, script_args))
 
